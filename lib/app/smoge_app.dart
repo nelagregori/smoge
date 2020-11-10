774768a5029e6bf1a6bc2ae423b781eec39b382b
nelagregori <35489547+nelagregori@users.noreply.github.com>
2020-11-10 17:01:12 +0100

    Home page

---
+++ lib/app/smoge_app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:smoge/ui/home/home_page.dart';
+
+class SmogeApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
